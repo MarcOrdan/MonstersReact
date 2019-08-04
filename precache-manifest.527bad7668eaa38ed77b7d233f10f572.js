7712ab93d9975296f848000fcf87bae381a10937
MarcOrdan <marcordan@gmail.com>
2019-08-04 15:11:56 +0800

    Updates

---
+++ precache-manifest.527bad7668eaa38ed77b7d233f10f572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954027d27d8f6fc45ade2b24ce6fc9f6",
+    "url": "/MonstersReact/index.html"
+  },
+  {
+    "revision": "e36f6363b92181ca9754",
+    "url": "/MonstersReact/static/css/main.203a9163.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/MonstersReact/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e36f6363b92181ca9754",
+    "url": "/MonstersReact/static/js/main.f7f2c1a1.chunk.js"
+  },
+  {
+    "revision": "4f1c0947833d91712cf7",
+    "url": "/MonstersReact/static/js/runtime~main.a7085db2.js"
+  }
+]);
